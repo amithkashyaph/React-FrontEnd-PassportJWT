76345a2f0614e625fbcb9b6e6147ef74cf9cfe38
Amith Kashyap H <amithkashyaprv@gmail.com>
2020-05-30 21:09:42 +0530

    React front-end for Passport-JWT app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./components/welcome";
+import { Switch, Route } from "react-router-dom";
+import RegisterForm from "./components/register";
+import LoginForm from "./components/login";
+import Home from "./components/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/register" component={RegisterForm} />
+        <Route path="/home" component={Home} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome Home!</h1>
+      <a class="btn btn-primary" href="http://localhost:5000/users/logout">
+        Logout
+      </a>
+    </div>
+  );
+};
+
+export default Home;
